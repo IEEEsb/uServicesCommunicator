ea222ae2f9689ba1197f6c9e856e758d4d78a769
charlascuen <carlos.cuencae@alumnos.upm.es>
2021-11-28 23:12:05 +0100

    add getAllServices

--- services/service.js
+++ services/service.js
@@ -3,3 +3,8 @@ const utils = require('../utils');
 module.exports.registerService = async (options) => {
 	await utils.sendRequest('service', 'POST', 'register', {}, options);
 };
+
+module.exports.getAllServices = async () => {
+	const { data } = await utils.sendRequest('service', 'GET', 'all');
+	return data?.services;
+};
