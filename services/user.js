3c20ff5ed09e3a88375aa3b97ded94324a8744f0
charlascuen <carlos.cuencae@alumnos.upm.es>
2021-11-28 10:58:44 +0100

    fix checkCredentials

--- services/user.js
+++ services/user.js
@@ -1,5 +1,6 @@
 const utils = require('../utils');
 
 module.exports.checkCredentials = async (email, password) => {
-	await utils.sendRequest('user', 'POST', 'checkCredentials', {}, { email, password });
+	const { data } = await utils.sendRequest('user', 'POST', 'checkCredentials', {}, { email, password });
+	return { userId: data.userId };
 };
